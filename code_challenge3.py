1a034d851a41f632905745cf4ebdbac2439f12f1
JmOrolfo <orolfojm08@gmail.com>
2025-08-27 11:10:59 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,11 @@
+from getpass import getpass
+uname = "Orolfojm08"
+pwd = "ingreso08"
+
+user = input("Create your User name :")
+passw = getpass("Enter password :" )
+if user == uname.lower() and passw == pwd:
+	print("Access granted")
+	print("Welcome to the Program")
+else:
+	print("Access denied")
