7e114422b6d3cb80b452193c55bd416599c08ab6
JmOrolfo <orolfojm08@gmail.com>
2025-09-22 11:28:06 +0800

    Add files via upload

---
+++ codechallenge10.py
@@ -0,0 +1,10 @@
+
+print("\t\t   *", end=" ")
+for u in range(1, 11, 1):
+    for e in range(11, u,-1):
+        print(" ", end=" ")
+    for o in range(1,u,1):
+            print("*" ,end=" ")
+    for i in range(1, u,1):
+        print("*", end=" ")
+    print()
