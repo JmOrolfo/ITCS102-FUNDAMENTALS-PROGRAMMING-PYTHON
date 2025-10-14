5bed0864a9c29d0451eda6a986904d78d8ee99ff
JmOrolfo <orolfojm08@gmail.com>
2025-10-14 14:37:41 +0800

    Update Cc7.py

--- Cc7.py
+++ Cc7.py
@@ -1,6 +1,6 @@
 i = 0
 for x in range(1, 11,1): 
-    y = eval(input("Put Number: "))
+    y = eval(input("Enter a Number: "))
     if y % 2:
         i += y
 print("The Sum of all odd number is",i) 
