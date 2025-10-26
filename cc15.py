f7de3d1848e776785adaff4b0091b5a8a24e77d1
JmOrolfo <orolfojm08@gmail.com>
2025-10-26 17:56:03 +0800

    Add files via upload

---
+++ cc15.py
@@ -0,0 +1,18 @@
+print("AniWantchlist")
+
+anime = []
+reyal = True
+
+while reyal == True:
+    wl = input("Enter the Anime you want to watch: ")
+    print("Anime has been added to your Watch List.")
+    print("Type exit when finished.")
+    anime.append(wl)
+    if wl == "exit":
+        print("Your listing is finish, exiting!")
+        anime.pop()
+        break
+
+print(f"Here is the Summation List of your Anime: ")
+for walist in anime:
+    print(f"- {walist}")
