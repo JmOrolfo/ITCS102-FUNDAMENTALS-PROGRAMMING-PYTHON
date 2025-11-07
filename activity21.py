90d70c13e707a24cc446d4b124003641e130513d
JmOrolfo <orolfojm08@gmail.com>
2025-11-07 11:16:26 +0800

    Add files via upload

---
+++ activity21.py
@@ -0,0 +1,14 @@
+notFull = True
+while notFull == True:
+    more=input('Do you want to add more ? (yes or no) :').lower()
+    if more == 'yes':
+        print("Keep Pouring")
+        continue
+    elif more == 'no':
+        print("Stopping...")
+        print("Drink Done." \
+        "")
+        break
+    else:
+        print("Invalid")
+        continue
---
+++ activity23.py
@@ -0,0 +1,17 @@
+li = ['ham', 'egg', 'bacon', 'sausage']#list
+li.append('hotdog')#Add
+print(li)
+li.pop() #remove
+print(li)
+
+
+for l in li:
+    print(f"{l} is already in the fridge")
+
+
+fname = 'Jay Lord Menk Orolfo'
+for fn in fname:
+    print(fn)
+print("\nReversed\n")
+for n in fname [::-1]:
+    print(n)
