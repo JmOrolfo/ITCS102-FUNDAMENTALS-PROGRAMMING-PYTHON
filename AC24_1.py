c8da3c92685693bb4e0af25d8ca4fdb274dc5e31
JmOrolfo <orolfojm08@gmail.com>
2025-11-07 11:16:00 +0800

    Add files via upload

---
+++ AC24_1.py
@@ -0,0 +1,6 @@
+from activity24 import greeter, summation
+
+greeter("Guevara")
+greeter("Baby ko")
+summation(30)
+summation(4)
---
+++ activity24.py
@@ -0,0 +1,16 @@
+def greeter(name):
+    print(f"Hello {name}, How are you ? ")
+
+def summation(num):
+    sum = 0
+    for j in range(num, 0, -1):
+        sum += j 
+    print(f" The summation of {num} is {sum}")
+
+greeter("Axel")
+greeter("Christian")
+greeter("Axel")
+
+summation(24)
+summation(8)
+summation(11)
