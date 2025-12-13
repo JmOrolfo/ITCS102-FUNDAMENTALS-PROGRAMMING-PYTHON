39ab951bbb7ed2b6df42c5d67fb50397a618f68a
JmOrolfo <orolfojm08@gmail.com>
2025-12-14 02:01:55 +0800

    Add user input functions for activities

---
+++ Activity25_1.py
@@ -0,0 +1,8 @@
+def activity1():
+  name = (input("Enter your name: "))
+def activity2():
+  se = (input("Male or Female? : ")
+def activity3():
+  age = (input("Enter your Age: "))
+def activity4():
+  sta = (input("Single or Married? : "))
