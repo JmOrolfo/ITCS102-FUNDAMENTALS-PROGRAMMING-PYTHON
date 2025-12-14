320bf7e0f99714c3802fcdd41b76a25a09914019
JmOrolfo <orolfojm08@gmail.com>
2025-12-14 20:06:28 +0800

    Add Christmas tree pattern generator
    This script generates a Christmas tree pattern using nested loops and print statements.

---
+++ CC13.py
@@ -0,0 +1,57 @@
+#CHRISTMAS TREE
+for i in range(2, 6, 1):
+    for iu in range(0, 7):
+        print(" ", end=" ")
+    for io in range(6, i, -1):
+        print(" ", end=" ")
+    for ia in range(3, i):
+        print("*", end=" ")
+    for ie in range(2, i, 1):
+        print("*", end=" ")
+    print()
+  
+for e in range(1, 3, 1):
+    for ei in range(0, 7):
+        print(" ", end=" ")
+    for eu in range(0 - 1, e, 1):
+        print(" ", end=" ")
+    for eo in range(2, e, -1):
+        print("*", end=" ")
+    for ea in range(3, e, -1):
+        print("*", end=" ")
+    print()
+    
+for u in range(2, 9, 1):
+    for ui in range(12, u, -1):
+        print(" ", end=" ")
+    for uo in range(2 , u, 1):
+        print("*", end=" ")
+    for ua in range(1, u):
+        print("*", end=" ")
+    print()
+    
+for o in range(2, 10, 1):
+    for oi in range(12, o, -1):
+        print(" ", end=" ")
+    for ou in range(1, o):
+        print("*", end=" ")
+    for oa in range(2, o):
+        print("*", end=" ")
+    print()
+    
+for a in range(2, 13, 1):
+    for ai in range(12, a, -1):
+        print(" ", end=" ")
+    for au in range(1, a):
+        print("*", end=" ")
+    for ao in range(2, a):
+        print("*", end=" ")
+    print()
+    
+for y in range(1, 7):
+    for yi in range(1, 9):
+        print(" ", end=" ")
+    for yu in range(1, 9):
+        print("", end="*")
+    print()   
+    
