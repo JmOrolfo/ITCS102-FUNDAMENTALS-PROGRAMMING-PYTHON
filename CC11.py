c052cabfddf3e902b963d34d8b6c5d416d141a5c
JmOrolfo <orolfojm08@gmail.com>
2025-12-14 19:12:43 +0800

    Add pattern printing script in CC11.py
    This script prints a pattern of asterisks in a specific format.

---
+++ CC11.py
@@ -0,0 +1,20 @@
+print("\t\t *")
+for i in range(2, 10, 1):
+  for u in range(10, i, -1):
+    print(" ", end=" ")
+  for o in range(1, i, 1):
+    print("*", end=" ")
+  for a in range(1, i, 1):
+    print ("*", end=" ")
+  print()
+
+for i in range (1, 10, 1):
+  for u in range(1, i, 1):
+    print(" ", end=" ")
+  for o in range(10, i, -1):
+    print("*", end=" ")
+  for a in range(10, i, -1):
+    print("*", end=" ")
+  print ()
+
+print("\t\t *")
