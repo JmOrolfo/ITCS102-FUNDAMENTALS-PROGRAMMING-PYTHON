4431f7e38dd9efc32fd32df230702aa549544440
JmOrolfo <orolfojm08@gmail.com>
2025-12-14 22:33:40 +0800

    Update Activity1.py

--- Activity1.py
+++ Activity1.py
@@ -1,3 +1,5 @@
+#Basic Personal Info
+
 Hi, I am Jay Lord Menk I. Orolfo, student from the BSIT First Year Section 1A
 
 
@@ -6,4 +8,5 @@ This is my first file in me REPO.
 About me:
 19, from brgy.4 Lucena City 
 I like playing Video Games and Guitar.
+
 
