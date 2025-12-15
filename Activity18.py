cac04a8379d4bd6dcff2d9a89148e92f10e23dbf
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:09:27 +0800

    Update Activity18.py

--- Activity18.py
+++ Activity18.py
@@ -1,6 +1,7 @@
-printing Half Pyramid
-
-for i in range(1,11):
-  for u in range(1,i):
-    print(u, end=" ")
-  print()
+#Printing Half Pyramid
+def Activity18():
+  for i in range(1,11):
+    for u in range(1,i):
+      print(u, end=" ")
+    print()
+    print('--------------------------------')
