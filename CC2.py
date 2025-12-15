3e65753058a9b6d371567d27775b38715b9993bb
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 19:02:09 +0800

    Update CC2.py

--- CC2.py
+++ CC2.py
@@ -1,32 +1,33 @@
-print(a , "= 1₱")
-print(b , "= 5₱")
-print(c , "= 10₱")
-print(d , "= 20₱")
-print(e , "= 50₱")
-print(f , "= 100₱")
-print(g , "= 200₱")
-print(h , "= 500₱")
-print(i , "= 1000₱")
-
-num = eval(input("Enter Amount : ")
-num = num % 1
-a = num // 1
-num = num % 5
-b = num // 5
-num = num % 10
-c = num // 10
-num = num % 20
-d = num // 20
-num = num % 50
-e = num // 50
-num = num % 100
-f = num // 100
-num = num % 200
-g = num // 200
-num = num % 500
-h = num // 500
-num = num % 1000
-i = num // 1000 
+def CC2():
+  print(a , "= 1₱")
+  print(b , "= 5₱")
+  print(c , "= 10₱")
+  print(d , "= 20₱")
+  print(e , "= 50₱")
+  print(f , "= 100₱")
+  print(g , "= 200₱")
+  print(h , "= 500₱")
+  print(i , "= 1000₱")
+  
+  num = eval(input("Enter Amount : ")
+  num = num % 1
+  a = num // 1
+  num = num % 5
+  b = num // 5
+  num = num % 10
+  c = num // 10
+  num = num % 20
+  d = num // 20
+  num = num % 50
+  e = num // 50
+  num = num % 100
+  f = num // 100
+  num = num % 200
+  g = num // 200
+  num = num % 500
+  h = num // 500
+  num = num % 1000
+  i = num // 1000 
 
 
 
