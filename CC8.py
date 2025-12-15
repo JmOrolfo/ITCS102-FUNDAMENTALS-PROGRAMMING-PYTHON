5b7eec6ca0bc16926271799d541062e829e49e33
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 19:05:25 +0800

    Update CC8.py

--- CC8.py
+++ CC8.py
@@ -1,7 +1,8 @@
 #Multiplication Table using for loop
-print("Multiplication Table")
-num = eval(input("Enter a Number: "))
-print("Multiplication Table For: ",num)
-for u in range(1 ,11):
-  total = num * u
-  print(num,"x",u,"=", total)
+def CC8():  
+  print("Multiplication Table")
+  num = eval(input("Enter a Number: "))
+  print("Multiplication Table For: ",num)
+  for u in range(1 ,11):
+    total = num * u
+    print(num,"x",u,"=", total)
