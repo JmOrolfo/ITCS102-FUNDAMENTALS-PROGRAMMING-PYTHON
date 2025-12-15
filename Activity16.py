28e7f2ac9a76e9f16c387759d8400e6322f03f09
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:08:25 +0800

    Update Activity16.py

--- Activity16.py
+++ Activity16.py
@@ -1,7 +1,10 @@
+
 #print HORIZONTALY
 #use (end="")
-for i in range(1,11,1):
-    print()
-    for x in range(1,11,1):
-        print(x,end=" ")
-        
+def Activity16():
+    for i in range(1,11,1):
+        print()
+        for x in range(1,11,1):
+            print(x,end=" ")
+        print('--------------------------------')
+            
