95213ee0e7f300e4c8b800e378390f5ce08bfe16
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 19:05:53 +0800

    Update CC9.py

--- CC9.py
+++ CC9.py
@@ -1,6 +1,7 @@
-print("Countdown AI")
-num = eval(input("Starting count: "))
-print("Countdown Start")
-for i in range(num, 0, -1):
-    print(i)
-print("Taking off...")   
+def CC9():
+    print("Countdown AI")
+    num = eval(input("Starting count: "))
+    print("Countdown Start")
+    for i in range(num, 0, -1):
+        print(i)
+    print("Taking off...")   
