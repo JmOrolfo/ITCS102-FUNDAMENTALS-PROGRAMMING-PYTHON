995f404fea31b0cf2b92c60209f177a5761a6581
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:13:25 +0800

    Update Activity22.py

--- Activity22.py
+++ Activity22.py
@@ -1,23 +1,30 @@
 #Number guessing
+def Activity22():
+    import random
 
-import random
+    num = random.randint(1,11)
 
-num = random.randint(1,10)
+    tries = 0
+    guess = True
 
-tries = 0
-guess = True
+    while guess == True:
+        g = int(input("What's your guess from 1-10 ? : "))
+        tries += 1
+        
+        if g == num:
+            print("CONGRATULATIONS!!!")
+            print(f"The answer is {num}")
+            print(f"You only took {tries} tries")
+            break
+        
+        else:
+            print("Wrong Guess")
+            stop = input("R - Retry\nS - Stop\nEnter here: ").upper()
+            if stop == 'R':
+                print("Try Again")
+                continue
 
-while guess == True:
-    g = int(input("What's your guess from ? : "))
-    tries += 1
-    
-    if guess == num:
-        print("CONGRATULATIONS!!!")
-        print(f"The answer is {num}")
-        print(f"You only took {tries} tries")
-        break
-    else:
-        print("Wrong Guess")
-        print("Try Again")
-        continue
-    
+            else:
+                print(f"The answer is {num}")
+                print("Thank you for playing")
+                break
