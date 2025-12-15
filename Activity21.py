36669bde8edc0b8dcca369383f1c6cbde4020b3e
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:11:23 +0800

    Update Activity21.py

--- Activity21.py
+++ Activity21.py
@@ -1,17 +1,18 @@
 #While loop
-
-notFull = True
-while notFull == True:
-    more=input('Do you want to add more ? (yes or no) :').lower()
-    if more == 'yes':
-        print("Keep Pouring")
-        continue
-    elif more == 'no':
-        print("Stopping...")
-        print("Drink Done." \
-        "")
-        break
-    else:
-        print("Invalid")
-        continue
+def Activity21():
+    notFull = True
+    while notFull == True:
+        more=input('Do you want to add more ? (yes or no) :').lower()
+        if more == 'yes':
+            print("Keep Pouring")
+            continue
+        elif more == 'no':
+            print("Stopping...")
+            print("Drink Done.")
+            break
+            print('------------------------------')
+        else:
+            print("Invalid")
+            continue
+        
 
