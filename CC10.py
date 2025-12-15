1b5c6f757198547472aecde5cd08568de39e1de6
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:58:09 +0800

    Update CC10.py

--- CC10.py
+++ CC10.py
@@ -1,12 +1,11 @@
-#PYRAMYD PATTERN
-print("\t\t   *", end=" ")
-for u in range(1, 11, 1):
-    for e in range(11, u,-1):
-        print(" ", end=" ")
-    for o in range(1,u,1):
-            print("*" ,end=" ")
-    for i in range(1, u,1):
-        print("*", end=" ")
+def CC10():
+    print("\t\t   *", end=" ")
+    for u in range(1, 11, 1):
+        for e in range(11, u,-1):
+            print(" ", end=" ")
+        for o in range(1,u,1):
+                print("*" ,end=" ")
+        for i in range(1, u,1):
+            print("*", end=" ")
 
-    print()
-
+        print()
