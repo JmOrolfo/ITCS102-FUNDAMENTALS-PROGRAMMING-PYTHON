9204d1e6c1e957d14f9fbdcaa0ab69d0f9615cf1
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:09:56 +0800

    Update Activity19.py

--- Activity19.py
+++ Activity19.py
@@ -1,6 +1,7 @@
 #Half Pyramid pattern
-
-for i in range(1,11):
-  for u in range(1, i, 1):
-    print("*", end="")
-  print()
+def Activity19():
+  for i in range(1,11):
+    for u in range(1, i, 1):
+      print("*", end="")
+    print()
+    print('--------------------------------')
