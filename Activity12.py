ea878afaadc74804f7c787a26a04888842b4c26e
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:06:06 +0800

    Update Activity12.py

--- Activity12.py
+++ Activity12.py
@@ -1,6 +1,7 @@
 #Basic looping
-
-print('Hello world')
-for ikot in range(1, 11):
-    print(ikot,'Madami' )
+def Activity12():
+    print('Hello world')
+    for ikot in range(1, 11):
+        print(ikot,'Madami' )
+    print('----------------------')
 
