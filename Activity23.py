5d5fa3fac86bd83beb3edfe97e7ffa2421cca309
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:14:02 +0800

    Update Activity23.py

--- Activity23.py
+++ Activity23.py
@@ -1,20 +1,22 @@
 #List Fundamentals and Iteration 
-
-li = ['ham', 'egg', 'bacon', 'sausage']#list
-li.append('hotdog')#Add
-print(li)
-li.pop() #remove
-print(li)
-
-
-for l in li:
-    print(f"{l} is already in the fridge")
+def Activity23():
+    li = ['ham', 'egg', 'bacon', 'sausage']#list
+    li.append('hotdog')#Add
+    print(li)
+    li.pop() #remove
+    print(li)
 
 
-fname = 'Jay Lord Menk Orolfo'
-for fn in fname:
-    print(fn)
-print("\nReversed\n")
-for n in fname [::-1]:
-    print(n)
+    for l in li:
+        print(f"{l} is already in the fridge")
+
+
+    fname = 'Jay Lord Menk Orolfo'
+    for fn in fname:
+        print(fn)
+    print("\nReversed\n")
+    for n in fname [::-1]:
+        print(n)
+        print('----------------------------------------------')
+
 
