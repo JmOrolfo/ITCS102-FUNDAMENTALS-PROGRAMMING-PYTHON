6eb292e7c2246f5222c0dd21f223c922b8c31d00
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 19:00:37 +0800

    Update CC15.py

--- CC15.py
+++ CC15.py
@@ -1,18 +1,19 @@
-print("AniWantchlist")
+def CC15():
+    print("AniWantchlist")
 
-anime = []
-reyal = True
+    anime = []
+    reyal = True
 
-while reyal == True:
-    wl = input("Enter the Anime you want to watch: ")
-    print("Anime has been added to your Watch List.")
-    print("Type exit when finished.")
-    anime.append(wl)
-    if wl == "exit":
-        print("Your listing is finish, exiting!")
-        anime.pop()
-        break
+    while reyal == True:
+        wl = input("Enter the Anime you want to watch: ")
+        print("Anime has been added to your Watch List.")
+        print("Type exit when finished.")
+        anime.append(wl)
+        if wl == "exit":
+            print("Your listing is finish, exiting!")
+            anime.pop()
+            break
 
-print(f"Here is the Summation List of your Anime: ")
-for walist in anime:
-    print(f"- {walist}")
+    print(f"Here is the Summation List of your Anime: ")
+    for walist in anime:
+        print(f"- {walist}")
