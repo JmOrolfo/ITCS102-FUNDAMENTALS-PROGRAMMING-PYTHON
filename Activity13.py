24617def8a18eaf9b95e81bf37cc97f8e12fadb3
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:06:37 +0800

    Update Activity13.py

--- Activity13.py
+++ Activity13.py
@@ -1,8 +1,8 @@
 #String Accumulation using 'loop'
-
-ct = "Lucena  "
-for x in range (1,21):
-     bg = input("What Baranggay are you from ? ")
-     ct += bg 
-     print("You're from", ct)
-
+def Activity13():
+     ct = "Lucena  "
+     for x in range (1,5):
+          bg = input("What Baranggay are you from ? ")
+          ct += bg 
+          print("You're from", ct)
+          print('-----------------------------------')
