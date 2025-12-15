754b75cf0adda64721029ef2dacd7afcae49d14a
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 19:04:47 +0800

    Update CC7.py

--- CC7.py
+++ CC7.py
@@ -1,7 +1,8 @@
 #SUMMATION OF ALL ODD NUMBERS
-i = 0
-for x in range(1, 11,1): 
-    y = eval(input("Enter a Number: "))
-    if y % 2:
-        i += y
-print("The Sum of all odd number is",i) 
+def CC7():
+    i = 0
+    for x in range(1, 11,1): 
+        y = eval(input("Enter a Number: "))
+        if y % 2:
+            i += y
+    print("The Sum of all odd number is",i) 
