507c1e79912c4c8c304840f646e952583b6c0d49
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:14:37 +0800

    Update Activity24.py

--- Activity24.py
+++ Activity24.py
@@ -1,19 +1,23 @@
 #Function Def and Reuse
+def Activity24():
+    import greeter
+    def greeter(name):
+        print(f"Hello {name}, How are you ? ")
 
-def greeter(name):
-    print(f"Hello {name}, How are you ? ")
+    def summation(num):
+        sum = 0
+        for j in range(num, 0, -1):
+            sum += j 
+        print(f" The summation of {num} is {sum}")
 
-def summation(num):
-    sum = 0
-    for j in range(num, 0, -1):
-        sum += j 
-    print(f" The summation of {num} is {sum}")
+    greeter("Axel")
+    greeter("Christian")
+    greeter("Axel")
 
-greeter("Axel")
-greeter("Christian")
-greeter("Axel")
+    summation(24)
+    summation(8)
+    summation(11)
+    print('----------------------------------------------')
 
-summation(24)
-summation(8)
-summation(11)
+
 
