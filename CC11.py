970d3d1ada265c142132c652e5d194e03230de7b
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:58:34 +0800

    Update CC11.py

--- CC11.py
+++ CC11.py
@@ -1,20 +1,21 @@
-print("\t\t *")
-for i in range(2, 10, 1):
-  for u in range(10, i, -1):
-    print(" ", end=" ")
-  for o in range(1, i, 1):
-    print("*", end=" ")
-  for a in range(1, i, 1):
-    print ("*", end=" ")
-  print()
+def CC11():
+  print("\t\t *")
+  for i in range(2, 10, 1):
+    for u in range(10, i, -1):
+      print(" ", end=" ")
+    for o in range(1, i, 1):
+      print("*", end=" ")
+    for a in range(1, i, 1):
+      print ("*", end=" ")
+    print()
 
-for i in range (1, 10, 1):
-  for u in range(1, i, 1):
-    print(" ", end=" ")
-  for o in range(10, i, -1):
-    print("*", end=" ")
-  for a in range(10, i, -1):
-    print("*", end=" ")
-  print ()
+  for i in range (1, 10, 1):
+    for u in range(1, i, 1):
+      print(" ", end=" ")
+    for o in range(10, i, -1):
+      print("*", end=" ")
+    for a in range(10, i, -1):
+      print("*", end=" ")
+    print ()
 
-print("\t\t *")
+  print("\t\t *")
