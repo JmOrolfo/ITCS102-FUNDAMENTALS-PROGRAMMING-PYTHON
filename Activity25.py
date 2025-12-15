085841dfa932176b86c845a93290865c4402a549
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:16:18 +0800

    Update Activity25.py

--- Activity25.py
+++ Activity25.py
@@ -1,36 +1,37 @@
 #Program Menu
+def Activity25():
+  from Activity1 import Activity1 
+  from Activity2 import Activity2
+  from Activity3 import Activity3
+  from Activity4 import Activity4
 
-from Activity_1 import *
+  name = input("Enter your name : ")
+  print(f"Welcome  {name}")
+  print("Please Select a Program")
+  print("A - Activity1\nB - Activity2\nC - Activity3\nD - Activity4\nE - Exit")
 
-name = input("Enter your name : ")
-print("Please Select a Program")
-print("A - Activity1\nB - Activity2\nC - Activity3\nD - Activity4\nE - Exit")
+  run = input("Enter which program you want to run : ").upper()
 
-run = input("Enter which program you want to run : ").lower()
+  if run == "A":
+    Activity1()
+    
+  elif run == "B":
+    Activity2()
+    
+  elif run == "C":
+    Activity3() 
 
-if run = "a":
-  activity1()
-  continue
+  elif run == "D":
+    Activity4()
 
-elif run = "b":
-  activity2()
-  continue
-  
-elif run = "c":
-  activity3()
-  continue
+  elif run == "E":
+    print("Exiting...")
+    print("Thankyou :)")
 
-elif run = "d":
-  activity4()
-  continue
-
-elif run = "e":
-  print("Exiting...")
-  print("Thankyou :)")
-  break
-
-else:
-  print("Invalid Input")
+  else:
+    print("Invalid Input")
+    print("-------------")
+    
 
 
 
