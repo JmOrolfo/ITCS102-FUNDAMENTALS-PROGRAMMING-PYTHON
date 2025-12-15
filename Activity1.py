b14fbfec6b59ca886ca0b8aa65260ed18a421df5
JmOrolfo <orolfojm08@gmail.com>
2025-12-15 18:03:10 +0800

    Update Activity1.py

--- Activity1.py
+++ Activity1.py
@@ -1,12 +1,11 @@
-#Basic Personal Info
+def Activity1():#Basic Personal Info
+    print("About me:")
+    print("Hi, Im Jay Lord Menk I. Orolfo, student from the BSIT First Year Section 1A")
+    print("This is my first file in me REPO.")
+    print("19, from brgy.4 Lucena City\nI like playing Video Games and Guitar")
+    print("------------------------------------------------------------------")
+    print("This is the code running for Activity 1")
 
-Hi, I am Jay Lord Menk I. Orolfo, student from the BSIT First Year Section 1A
 
-
-This is my first file in me REPO.
-
-About me:
-19, from brgy.4 Lucena City 
-I like playing Video Games and Guitar.
 
 
