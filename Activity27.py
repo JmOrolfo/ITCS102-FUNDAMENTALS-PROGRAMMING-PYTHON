b4bfd7829e5e34d58fc53b915a6a417e50ab4643
JmOrolfo <orolfojm08@gmail.com>
2025-12-16 19:52:02 +0800

    Update Activity27.py

--- Activity27.py
+++ Activity27.py
@@ -1,55 +1,51 @@
 #Anime Watch list Dictionary
-def Activity27():
-    print('Adding anime in watchlist')
+#def Activity27():
+print('Adding anime in watchlist')
 
-    li = {}
+li = {}
 
-    true = True
+true = True
 
-    def Print_Anime():
-        for i,j in li.items():
-            print(f'for Anime {j}, the key is {i}')
+def Print_Anime():
+    for i,j in li.items():
+        print(f'for Anime {j}, the key is {i}')
 
-    def Search_Anime(key):
-        print('Searching...')
-        print(f"result shows {li[key]} is in your watchlist.")
+def Search_Anime(code):
+    print('Searching...')
+    if code in li:
+        print(f"Result shows {li[code]} is in your watchlist.")
+    else:
+        print(f"Key '{code}' not found. That anime is not in your watchlist.")
 
-    while true == True:
-        key = input('Input anime keys :')
-        tt = input('Enter anime name :')
+while true == True:
+    key = input('Input anime keys :')
+    tt = input('Enter anime name :')
 
-        li[key] = tt
+    li[key] = tt
 
-        choice = input('Would you like to continue adding? \nY - Yes\nN - No\nP - Print\nS - Search\nENTER: ').lower()
+    choice = input('Would you like to continue adding? \nY - Yes\nN - No\nP - Print\nS - Search\nENTER: ').lower()
 
-        if choice == 'y':
-            print('has been added to your watchlist.')
-            print('continuing...')
-            continue
+    if choice == 'y':
+        print('has been added to your watchlist.')
+        print('continuing...')
+        continue
 
-        elif choice == 'n':
-            print('Exiting...')
-            print('Buh-bye.')
-            break
-        
-        elif choice == 'p':
-            print('Printing.....')
-            Print_Anime()
-            continue
-        
-        elif choice == 's':
-            code = input('Input the anime key.').lower()
-            Search_Anime(key)
-            continue
-        
-        else:
-            print('Error')
-            break
-
-
+    elif choice == 'n':
+        print('Exiting...')
+        print('Buh-bye.')
+        break
+    
+    elif choice == 'p':
+        print('Printing.....')
+        Print_Anime()
+        continue
+    
+    elif choice == 's':
+        code = input('Input the anime key.').lower()
+        Search_Anime(code)
+        continue
     
     else:
         print('Error')
         break
-
 
