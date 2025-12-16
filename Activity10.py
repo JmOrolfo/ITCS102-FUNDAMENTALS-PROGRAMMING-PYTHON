e4fc16c698f1eac456e19643fea19e2af96f74ad
JmOrolfo <orolfojm08@gmail.com>
2025-12-16 17:03:22 +0800

    Update Activity10.py

--- Activity10.py
+++ Activity10.py
@@ -1,22 +1,21 @@
 #Discount Calculation using 'if/else'
-def Activity10():
-	print("---------------------------")
-	print("--WELCOME to Item Counter--")
-	print("---------------------------")
+# def Activity10():
+print("---------------------------")
+print("--WELCOME to Item Counter--")
+print("---------------------------")
 
-	name = input("What is your Name? :")
-	item = input("What item are you buying? (COFFEE/MILK) : ")
-	no = eval(input("How many(1-10): "))
-	prc = eval(input("How much? "))
-	pwd = input("are you PWD or Senior(Yes/No): ")
-	total = no * prc * .05
-	tot = no * prc
-	disc = tot - no
+name = input("Enter name: ")
+item = input("Item: ")
+no = eval(input("Quantity: "))
+prc = eval(input("Price Each: "))
+pwd = input("are you PWD or Senior(Yes/No): ")
+subtot = no * prc
+disc = 0
 
-	print("---------------------------")
-	print("---------------------------")
-	if pwd.lower() == "yes":
-		print("Hello", name, "\nItem: ",item, "\nQuantity: ",no,"\nPrice each: ",prc,"\nTotal: ",tot ,"\nDiscount:",total,"\nTotal w/discount: ",disc)
-	else:
-		print("Hello", name, "\nItem: ",item, "\nQuantity: ",no,"\nPrice each: ",prc,"\nTotal: ",tot ,"\nDiscount:","\nTotal w/discount: ",tot)
-	print("----------------------------------------------------------------------------------------------------------------------------------")
+if pwd.lower() == "yes":
+	disc_am = subtot * 0.05
+	total_with_disc = subtot - disc_am
+	print("Hello", name, "\nItem: ",item, "\nQuantity: ",no,"\nPrice each: ",prc,"\nTotal: ",subtot ,"\nDiscount:",disc_am,"\nTotal w/discount: ", total_with_disc)
+else:
+	total_with_disc = subtot 
+	print("Hello", name, "\nItem: ",item, "\nQuantity: ",no,"\nPrice each: ",prc,"\nTotal: ",subtot ,"\nDiscount:","\nTotal w/discount: ", total_with_disc)
