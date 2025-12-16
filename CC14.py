b5288072baea3899da111174db959fc3bfeeed12
JmOrolfo <orolfojm08@gmail.com>
2025-12-16 19:49:50 +0800

    Update CC14.py

--- CC14.py
+++ CC14.py
@@ -1,22 +1,24 @@
-def CC14():
-    name = input("Enter your name: ")
+#def CC14():
+name = input("Enter your name: ")
+print("Input 0 to stop program")
 
-    odd = 0
-    num = ""
-    yes = True
+odd = 0
+num = ""
+yes = True
 
-    while yes == True:
-        nom = eval(input("Put a number: "))
-        if nom % 2 == 1:
-            print("ODD number detected")
-            odd += nom
-            num += str(nom) + ","
-        elif nom == 0:
-            print("End")
-            break
-        else:
-            print("EVEN number detected!")
-            print("Skipping...")
-            continue
-    print(f"Total of all ODD numbers: {odd}")
-    print(f"All ODD numbers: {num}")
+while yes == True:
+    nom = eval(input("Put a number: "))
+    if nom % 2 == 1:
+        print("ODD number detected")
+        odd += nom
+        num += str(nom) + ","
+    elif nom == 0:
+        print("End")
+        break
+    else:
+        print("EVEN number detected!")
+        print("Skipping...")
+        continue
+print(f"Total of all ODD numbers: {odd}")
+print(f"All ODD numbers: {num}")
+
